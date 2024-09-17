83cdf9d0feef836ea27a7505eb0d1c18ce3b273c
Imron jehleh <imron.jehleh@gmail.com>
2024-09-17 11:25:18 +0700

    Set function to public.

--- #6 20240917/CPE3243.java
+++ #6 20240917/CPE3243.java
@@ -3,12 +3,12 @@ class CPE3243
     private int x_value;
     private int y_value;
 
-    int setX_value(int m)
+    public int setX_value(int m)
     {
         x_value = m;
     }
 
-    int getX_value()
+    public int getX_value()
     {
         return x_value;
     }
