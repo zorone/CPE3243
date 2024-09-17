2eb6fc7429a6b558312202a2df7ad696460de2f9
Imron jehleh <imron.jehleh@gmail.com>
2024-09-17 11:21:37 +0700

    Urgently initialize git. Add class 6, 20240917.

---
+++ #6 20240917/CPE3243.java
@@ -0,0 +1,17 @@
+class CPE3243
+{
+    int x;
+    int y;
+
+    int plus(int m, int n)
+    {
+        return m+n;
+    }
+
+    public static void main(String[] args)
+    {
+        System.ou.println("Hello World!");
+    }
+}
+
+
